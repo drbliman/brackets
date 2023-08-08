4598572154fb26b1e3292cf4621c95851d0569ad
drbliman <dr.bliman505@gmail.com>
2023-08-08 12:12:37 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  console.log(str);
+  for (let i=0; i<bracketsConfig.length; i++) {
+      for (let j=0; j<str.length; j++) {
+          if (str[j] == bracketsConfig[i][0] &&
+              str[j + 1] == bracketsConfig[i][1]) {
+                  str.splice(j, 2);
+                  j = j - 2;
+                  i = 0;
+          }
+      }
+  }
+  return str.length == 0 ? true : false
 }
